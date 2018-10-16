a5871a96098e8d760b5f3e669efed076a7727da0
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-10-15 22:39:44 -0500

    revised responsiveness

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const jimp = require("gulp-jimp-resize");
+const image = require('gulp-image');
+
+// Resize and Optimize Images
+gulp.task('image', function () {
+    gulp.src('./img/*.jpg')
+        .pipe(jimp({
+            sizes: [{
+                "width": 500
+            }]
+        }))
+        .pipe(image())
+        .pipe(gulp.dest('img'));
+});
+
+gulp.task('default', ['image']);
--- js/main.js
+++ js/main.js
@@ -168,9 +168,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = "Image of the restaurant " + restaurant.name;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -183,9 +184,8 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
-  more.innerHTML = 'View Details';
+  more.innerHTML = 'View Details of '+ restaurant.name;
   more.href = DBHelper.urlForRestaurant(restaurant);
-  more.tabIndex=3;
   li.append(more)
 
   return li
